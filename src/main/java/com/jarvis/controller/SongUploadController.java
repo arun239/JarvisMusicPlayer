bb8564453e688dfa9f077d34f285634c100cf428
arun239 <gupta.aruninvit@gmail.com>
2016-01-14 13:10:11 +0530

    Upload Song functionality is working fine now

--- src/main/java/com/jarvis/controller/SongUploadController.java
+++ src/main/java/com/jarvis/controller/SongUploadController.java
@@ -1,9 +1,11 @@
 package com.jarvis.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.BufferedOutputStream;
@@ -16,6 +18,7 @@ import java.io.FileOutputStream;
 
 @RestController
 public class SongUploadController {
+    private static final Logger logger = LoggerFactory.getLogger(SongUploadController.class);
 
     @RequestMapping(value="/SongUpload", method = RequestMethod.GET)
     public String songUploadInfo() {
@@ -24,18 +27,25 @@ public class SongUploadController {
     }
 
     @RequestMapping(value="/SongUpload", method = RequestMethod.POST)
+
     public String songUploadHandler(@RequestParam("name") String name,
                                     @RequestParam("Song") MultipartFile Song) {
 
         if (!Song.isEmpty()) {
             try {
+                logger.info("Checkpoint - 1");
                 byte[] bytes = Song.getBytes();
+                logger.info("Checkpoint - 2");
+                File file = new File("C:/" + Song.getOriginalFilename());
+                logger.info("Checkpoint - 2.5");
                 BufferedOutputStream stream =
-                        new BufferedOutputStream(new FileOutputStream(new File(name)));
+                        new BufferedOutputStream(new FileOutputStream(file));
+                logger.info("Checkpoint - 3");
                 stream.write(bytes);
                 stream.close();
                 return "You successfully uploaded " + name + "!";
             } catch (Exception e) {
+                logger.error(e.toString());
                 return "You failed to upload " + name + " => " + e.getMessage();
             }
         } else {
